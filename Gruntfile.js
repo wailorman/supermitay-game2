266b9418186acd5032832d43439dc482b7eb91b3
wailorman <wailorman@gmail.com>
2015-01-19 15:30:11 +0300

    Modify Grunt task: requirejs, less, copy, cssmin, htmlbuild, clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,67 @@ module.exports = function ( grunt ) {
                     baseUrl:                '.',
                     name:                   'main',
                     mainConfigFile:         "./main.js",
-                    out:                    "app.build.js",
+                    out:                    "built/app.build.js",
                     findNestedDependencies: true,
                     include:                [ 'bower_components/requirejs/require.js' ],
                     optimize:               'uglify'
                 }
             }
-        }
+        },
+        copy:      {
+            sprites: {
+                src:  'sprites/*',
+                dest: 'built/'
+            },
+            music:   {
+                src:  'bg.mp3',
+                dest: 'built/'
+            }
+        },
 
+        htmlbuild: {
+            stable: {
+                src:     'index.html',
+                dest:    'built/',
+                options: {
+                    scripts: {
+                        appBuild: 'built/app.build.js'
+                    },
+                    styles:  {
+                        concatCss: 'built/build.css'
+                    }
+                }
+            }
+        },
+
+        cssmin: {
+            stable: {
+                files: {
+                    'built/build.css': [ 'bower_components/bootstrap/dist/css/bootstrap.min.css', 'css/main.css' ]
+                }
+            }
+        },
+
+        less: {
+            compile: {
+                files: {
+                    "css/main.css": "css/main.less"
+                }
+            }
+        },
+
+        clean: {
+            compile: [ 'built/build.css', 'built/app.build.js' ]
+        }
     } );
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks( 'grunt-contrib-less' );
+    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+    grunt.loadNpmTasks( 'grunt-html-build' );
     grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
+    grunt.loadNpmTasks( 'grunt-contrib-copy' );
+
+    grunt.task.registerTask( 'default', [ 'requirejs', 'less', 'copy', 'cssmin', 'htmlbuild', 'clean' ] );
 
 };
